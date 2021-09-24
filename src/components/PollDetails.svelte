0edb6cf6f601aff1fa2668a4d591dd47f1b790f4
trevor <me@trevorbehnke.com>
2021-09-24 15:18:48 -0700

    configured delete functionality

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -2,6 +2,7 @@
   import Card from '../shared/Card.svelte';
   import PollStore from "../stores/PollStore";
   export let poll;
+  import Button from "../shared/Button.svelte"
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
@@ -26,6 +27,15 @@
       return copiedPolls;
     })
   };
+
+  // delete a polll
+  const handleDelete = (id) => {
+    PollStore.update(currentPolls => {
+      return currentPolls.filter(poll => 
+        poll.id != id
+      )
+    })
+  };
 </script>
 
 <Card>
@@ -41,6 +51,11 @@
       <span>{ poll.answerB } ({ poll.votesB } votes)</span>
     </div>
   </div>
+  <div class="delete">
+    <Button flat={true} on:click={()=> handleDelete(poll.id)}>Delete</Button>
+  </div>
+
+ 
 </Card>
 
 <style>
@@ -79,5 +94,9 @@
   .percent-b {
     border-left: 4px solid #45c496;
     background: rgba(69,196,150,0.2);
+  }
+  .delete {
+    margin-top: 30px;
+    text-align: center ;
   }
 </style>
